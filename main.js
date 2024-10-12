ab7366190d13ec84f5603872a7e9348bfc374972
Mario Salles <mcfsalles@hotmail.com>
2024-10-12 01:48:39 +0100

    static: adds solution for challenge-23

--- main.js
+++ main.js
@@ -1,122 +1,142 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    //     var myName = 'Mario Salles';
-    //     console.log(myName)
+/** Seção debug */
 
-    /** Seção ?? */
-    //     var obj = {
-    //         prop1: {
-    //             prop2: {
-    //                 prop3: {
-    //                     k1: 'v1',
-    //                     k2: 'v2',
-    //                     k3: 'v3'
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     console.log(obj.prop1.prop2.prop3);
-    //     console.log('----------------');
-    //     with (obj.prop1.prop2.prop3) { // a interpretação devido o use stritct é impedida a partir daqui
-    //         console.log(k1, k2, k3);
-    //     }
+  // var arr = [
+  //   { item: 'Arroz', preco: 10, weight: 5 },
+  //   { item: 'Feijão', preco: 5, weight: 5 },
+  //   { item: 'Macarrão', preco: 7, weight: 5 },
+  //   { item: 'Carne', preco: 30, weight: 5 },
+  // ];
+
+  // console.table(arr);
 
 
-    /** Seção ?? */
-    //     console.log(this);
-    //     function Person(name, lastName, age) {
-    //         this.name = name;
-    //         this.lastName = lastName;
-    //         this.age = age;
-    //     }
-    //     console.log(new Person('Mario', 'Salles', 43)); // chamada correta
-    //     console.log( Person('Mario', 'Salles', 43)); // da erro
+  // function sum() {
+  //   return Array.prototype.reduce.call(arguments, function (acumulated, actualItem) {
+  //     return Number(acumulated) + Number(actualItem);
+  //   });
+  // }
+  // console.log(sum(1, 2, 3, 4, 5));
+
+  //     var myName = 'Mario Salles';
+  //     console.log(myName)
+
+  /** Seção ?? */
+  //     var obj = {
+  //         prop1: {
+  //             prop2: {
+  //                 prop3: {
+  //                     k1: 'v1',
+  //                     k2: 'v2',
+  //                     k3: 'v3'
+  //                 }
+  //             }
+  //         }
+  //     }
+  //     console.log(obj.prop1.prop2.prop3);
+  //     console.log('----------------');
+  //     with (obj.prop1.prop2.prop3) { // a interpretação devido o use stritct é impedida a partir daqui
+  //         console.log(k1, k2, k3);
+  //     }
 
 
-    /** Seção ?? */
-
-    //     var var1 = 'Teste';
-    //     var obj = {
-    //         k1: 'v1',
-    //         k1: 'v2',
-    //         k3: 'v3'
-    //     };
-    //     console.log('antes do delete', obj);
-    //     console.log(delete obj.k1);
-    //     console.log('apos delete: ', obj);
-    //     console.log('var1: ', var1);
-    //     // console.log('tenta deletar var1', delete var1); // sem o use strict retorna false, com strict nem interpreta
+  /** Seção ?? */
+  //     console.log(this);
+  //     function Person(name, lastName, age) {
+  //         this.name = name;
+  //         this.lastName = lastName;
+  //         this.age = age;
+  //     }
+  //     console.log(new Person('Mario', 'Salles', 43)); // chamada correta
+  //     console.log( Person('Mario', 'Salles', 43)); // da erro
 
 
-    /** Seção 22 */
+  /** Seção ?? */
+
+  //     var var1 = 'Teste';
+  //     var obj = {
+  //         k1: 'v1',
+  //         k1: 'v2',
+  //         k3: 'v3'
+  //     };
+  //     console.log('antes do delete', obj);
+  //     console.log(delete obj.k1);
+  //     console.log('apos delete: ', obj);
+  //     console.log('var1: ', var1);
+  //     // console.log('tenta deletar var1', delete var1); // sem o use strict retorna false, com strict nem interpreta
 
 
-    // function myFunction(a, b, c) {
-    //     console.log(a, b, c);
-    // }
+  /** Seção 22 */
 
-    // function myFunction2(a, b, c) {
-    //     console.log(this.lastName, a, b, c);
-    // }
 
-    // console.log('myfunc length, retorna o \nnumero de argumentos da função: ', myFunction.length);
+  // function myFunction(a, b, c) {
+  //     console.log(a, b, c);
+  // }
 
-    // var arr = [1, 2, 3, 4, 5];
-    // var obj = { prop1: 1, prop2: 2, prop3: 3 };
+  // function myFunction2(a, b, c) {
+  //     console.log(this.lastName, a, b, c);
+  // }
 
-    // console.log('array: ', arr);
-    // console.log('array to string', arr.toString());
+  // console.log('myfunc length, retorna o \nnumero de argumentos da função: ', myFunction.length);
 
-    // console.log('obj: ', obj);
-    // console.log('obj to string', obj.toString());
+  // var arr = [1, 2, 3, 4, 5];
+  // var obj = { prop1: 1, prop2: 2, prop3: 3 };
 
-    // console.log('myFunction to string', myFunction.toString());
+  // console.log('array: ', arr);
+  // console.log('array to string', arr.toString());
 
-    // console.log('myFunction call:');
-    // myFunction.call(myFunction, 1, 2, 3); // chama a função com o this apontando para a função
+  // console.log('obj: ', obj);
+  // console.log('obj to string', obj.toString());
 
-    // console.log('myFunction2 call com this obj e arr:');
-    // myFunction2.call({ lastName: 'Call 1' }, [1, 2, 3, 4, 5, 6]);
+  // console.log('myFunction to string', myFunction.toString());
 
-    // console.log('myFunction2 call com this obj e ...arr:');
-    // myFunction2.call({ lastName: 'Call...' }, ...[1, 2, 3, 4, 5, 6]);
+  // console.log('myFunction call:');
+  // myFunction.call(myFunction, 1, 2, 3); // chama a função com o this apontando para a função
 
-    // console.log('myFunction2 apply com this obj e arr:');
-    // myFunction2.apply({ lastName: 'Apply' }, [1, 2, 3, 4, 5, 6]);
+  // console.log('myFunction2 call com this obj e arr:');
+  // myFunction2.call({ lastName: 'Call 1' }, [1, 2, 3, 4, 5, 6]);
 
-    // function MyFunction3(name, lastName) {
-    //     this.name = name;
-    //     this.lastName = lastName;
-    //     this.age = 43;
-    // }
+  // console.log('myFunction2 call com this obj e ...arr:');
+  // myFunction2.call({ lastName: 'Call...' }, ...[1, 2, 3, 4, 5, 6]);
 
-    // MyFunction3.prototype.fullName = function () {
-    //     return this.name + ' ' + this.lastName;
-    // }
+  // console.log('myFunction2 apply com this obj e arr:');
+  // myFunction2.apply({ lastName: 'Apply' }, [1, 2, 3, 4, 5, 6]);
 
-    // var mario = new MyFunction3('Mario', 'Salles');
-    // console.log(mario.fullName());
+  // function MyFunction3(name, lastName) {
+  //     this.name = name;
+  //     this.lastName = lastName;
+  //     this.age = 43;
+  // }
 
-    // MyFunction3.prototype.age = 40;
-    // console.log(mario.age);
+  // MyFunction3.prototype.fullName = function () {
+  //     return this.name + ' ' + this.lastName;
+  // }
 
-    function ArrayElements() {
-        var result = Array.prototype.reduce.call(
-            arguments,
-            function (accu, curr, _) {
-                return accu + curr;
-            },
-        );
-        console.log(result);
-    }
-    
-    ArrayElements(1, 2, 3, 4, 5, 6, 7, 8);
+  // var mario = new MyFunction3('Mario', 'Salles');
+  // console.log(mario.fullName());
 
-    function ArrayLikeFunc() {
-        console.log(arguments);
-    }
-    ArrayLikeFunc(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  // MyFunction3.prototype.age = 40;
+  // console.log(mario.age);
+
+  // function ArrayElements() {
+  //     var result = Array.prototype.reduce.call(
+  //         arguments,
+  //         function (accu, curr, _) {
+  //             return accu + curr;
+  //         },
+  //     );
+  //     console.log(result);
+  // }
+
+  // ArrayElements(1, 2, 3, 4, 5, 6, 7, 8);
+
+  // function ArrayLikeFunc() {
+  //     console.log(arguments);
+  // }
+  // ArrayLikeFunc(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
 
 
 
