860a9504042fb269f56b13ac241e7f473da0179a
Mario Salles <mcfsalles@hotmail.com>
2024-12-09 09:50:27 +0000

    improv: adds frontend

---
+++ challenge-32/front/dom.js
@@ -0,0 +1,35 @@
+(function (win, document) {
+  'use strict';
+
+  function DOM(elements) {
+    if (!(this instanceof DOM)) return new DOM(elements);
+    this.element = document.querySelectorAll(elements);
+  }
+
+  DOM.prototype.on = function (eventType, callback) {
+    this.forEach(element => element.addEventListener(eventType, callback, false));
+  };
+
+  DOM.prototype.off = function (eventType, callback) {
+    this.forEach(element => element.removeEventListener(eventType, callback, false));
+  };
+
+  DOM.prototype.get = function (index) {
+    if (!index) return this.element[0];
+    return this.element[index];
+  };
+
+  ['forEach', 'map', 'filter', 'reduce', 'reduceRight', 'every', 'some'].forEach(function (method) {
+    DOM.prototype[method] = function () {
+      return Array.prototype[method].apply(this.element, arguments);
+    };
+  });
+
+  ['Array', 'Object', 'Function', 'Number', 'String', 'Boolean', 'Null'].forEach(function (type) {
+    DOM['is' + type] = function (param) {
+      return Object.prototype.toString.call(param) === `[object ${type}]` || (type === 'Null' && param == null);
+    };
+  });
+
+  win.DOM = DOM;
+})(window, document);
